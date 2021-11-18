ac5175be860b1e951933ed89f2a682fdf31631d6
Ivan Nikolaev-Axenov <nikolaevaxenov@gmail.com>
2021-11-19 01:34:21 +0300

    Добавлен файл Procfile

---
+++ src/database/migrations/2021_11_18_182707_create_games_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->uuid('id')->unique();
+            $table->timestamps();
+            $table->string('field', 9)->default("111111111");
+            $table->string('player1');
+            $table->string('player2');
+            $table->boolean('isOpenGame')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
